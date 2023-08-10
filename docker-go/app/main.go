b7839505423ad1f8d42dfabfb69cc67c25e7067a
Damian Kolaska <damian.kolaska99@gmail.com>
2023-08-10 19:31:53 +0200

    docker-go: Finished stages 1-4

---
+++ docker-go/app/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+  "syscall"
+)
+
+// Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
+func main() {
+	command := os.Args[3]
+	args := os.Args[4:len(os.Args)]
+
+	cmd := exec.Command(command, args...)
+  
+  cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+  if err := syscall.Chroot("./chroot-jail"); err != nil {
+			panic(err)
+	}
+
+  err := cmd.Run()
+
+	if err != nil {
+    e := strings.Split(err.Error(), " ")
+		errNum, _ := strconv.Atoi(e[len(e)-1])
+		os.Exit(errNum)
+	}
+}
